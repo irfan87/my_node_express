1cd3df879ab0890d8d70689256fe812d1f975088
irfan87 <nerve2009@yahoo.com>
2016-02-26 06:06:33 +0800

    update express

--- express_tut.js
+++ express_tut.js
@@ -1,12 +1,28 @@
 var express = require('express');
+
 var app = express();
 
+app.disable('x-powered-by');
+
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+
+app.engine('handlebars', handlebars.engine);
+
+app.set('view engine', 'handlebars');
+
+// more imports here
 app.set('port', process.env.PORT || 3000);
 
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', function(req, res){
-	res.send('Express Works!');
+	res.render('home');
+});
+
+app.get('/about', function(req, res){
+	res.render('about');
 });
 
 app.listen(app.get('port'), function(){
-	console.log('Express Started. Press Ctrl+C to terminate');
+	console.log("Express started on http://localhost:" + app.get('port') + ". Press CTRL-C to terminate.");
 });
