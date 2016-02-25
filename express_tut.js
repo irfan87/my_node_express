66816961af74bddc7eb5941843cdd65b825206d2
irfan87 <nerve2009@yahoo.com>
2016-02-26 06:53:00 +0800

    update contact second time

--- express_tut.js
+++ express_tut.js
@@ -10,7 +10,14 @@ app.engine('handlebars', handlebars.engine);
 
 app.set('view engine', 'handlebars');
 
-// more imports here
+app.use(require('body-parser').urlencoded({extended: true}));
+
+var formidable = require('formidable');
+
+var credentials = require('./credentials.js');
+
+app.use(require('cookie-parser')(credentials.cookieSecret));
+
 app.set('port', process.env.PORT || 3000);
 
 app.use(express.static(__dirname + '/public'));
@@ -40,7 +47,20 @@ app.get('/about', function(req, res){
 });
 
 app.get('/contact', function(req, res){
-	res.render('contact');
+	res.render('contact', {csrf: 'CSRF token here'});
+});
+
+app.get('/thankyou', function(req, res){
+	res.render('thankyou');
+});
+
+app.post('/process', function(req, res){
+	console.log('Form: ' + req.query.form);
+	console.log('CSRF token: ' + req.body._csrf);
+	console.log('Email: ' + req.body.email);
+	console.log('Question: ' + req.body.ques);
+
+	res.redirect(303, '/thankyou');
 });
 
 app.use(function(req, res){
@@ -54,6 +74,8 @@ app.use(function(req, res, err, next){
 	res.status(500);
 	res.render('500');
 });
+
+
 
 app.listen(app.get('port'), function(){
 	console.log("Express started on http://localhost:" + app.get('port') + ". Press CTRL-C to terminate.");
